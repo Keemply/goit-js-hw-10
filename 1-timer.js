7cf2e54600930c6c37e799ce21f99a2aba28cf4e
Keemply <144055348+Keemply@users.noreply.github.com>
2024-09-13 16:55:28 +0000

    Deploying to gh-pages from @ Keemply/goit-js-hw-10@f5ef68b63adb2699b6132b022a0e30c5c30ade5f 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as o}from"./assets/vendor-EyZmBGcZ.js";const l={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){console.log(t[0])}},e=document.querySelector("#datetime-picker");o(e,l);console.log(e);let n=selectedDates[0];console.log(n);
+//# sourceMappingURL=1-timer.js.map
