a2af89eb482abf9488e22addf53c1bf950225c78
Keemply <144055348+Keemply@users.noreply.github.com>
2024-09-17 17:16:07 +0300

    finish

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,48 @@
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
 console.log('bar');
+const form = document.querySelector('.form');
+const input = form.children[0].children[0];
+const snackBar = {
+  inputValue: null,
+};
+function inputHandler() {
+  snackBar.inputValue = input.value;
+  console.log(input.value);
+  console.log(form);
+}
+function submitHandler(event) {
+  event.preventDefault();
+  const check = event.originalTarget[2].checked;
+  form.reset();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (check) {
+        resolve(
+          iziToast.show({
+            message: `✅ Fulfilled promise in ${snackBar.inputValue}ms`,
+            backgroundColor: 'red',
+            position: 'topRight',
+            messageColor: 'white',
+            progressBar: false,
+            close: false,
+          })
+        );
+      } else {
+        reject(
+          iziToast.show({
+            message: `❌ Rejected promise in ${snackBar.inputValue}ms`,
+            backgroundColor: 'red',
+            position: 'topRight',
+            messageColor: 'white',
+            progressBar: false,
+            close: false,
+          })
+        );
+      }
+    }, snackBar.inputValue);
+  });
+}
+input.addEventListener('input', inputHandler);
+form.addEventListener('submit', submitHandler);
