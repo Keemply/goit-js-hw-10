b0656b455a9a3367f1675d5a8a8be73a3eef93d0
Keemply <144055348+Keemply@users.noreply.github.com>
2024-09-17 22:35:24 +0300

    again

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,48 +1,66 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-console.log('bar');
 const form = document.querySelector('.form');
-const input = form.children[0].children[0];
-const snackBar = {
-  inputValue: null,
-};
-function inputHandler() {
-  snackBar.inputValue = input.value;
-  console.log(input.value);
-  console.log(form);
-}
+// if (check) {
+//     resolve();
+//     iziToast.show({
+//       message: `✅ Fulfilled promise in ${delay}ms`,
+//       backgroundColor: 'red',
+//       position: 'topRight',
+//       messageColor: 'white',
+//       progressBar: false,
+//       close: false,
+//     });
+//   } else {
+//     reject();
+//     iziToast.show({
+//       message: `❌ Rejected promise in ${delay}ms`,
+//       backgroundColor: 'red',
+//       position: 'topRight',
+//       messageColor: 'white',
+//       progressBar: false,
+//       close: false,
+//     });
+//   }
+// }
+
 function submitHandler(event) {
   event.preventDefault();
-  const check = event.originalTarget[2].checked;
-  form.reset();
-  return new Promise((resolve, reject) => {
+  const check = event.target[2].checked;
+  const delay = event.target[0].value;
+
+  new Promise((resolve, reject) => {
     setTimeout(() => {
       if (check) {
-        resolve(
-          iziToast.show({
-            message: `✅ Fulfilled promise in ${snackBar.inputValue}ms`,
-            backgroundColor: 'red',
-            position: 'topRight',
-            messageColor: 'white',
-            progressBar: false,
-            close: false,
-          })
-        );
+        resolve(exect(check, delay));
       } else {
-        reject(
-          iziToast.show({
-            message: `❌ Rejected promise in ${snackBar.inputValue}ms`,
-            backgroundColor: 'red',
-            position: 'topRight',
-            messageColor: 'white',
-            progressBar: false,
-            close: false,
-          })
-        );
+        reject(exect(check, delay));
       }
-    }, snackBar.inputValue);
+    }, delay);
   });
+  form.reset();
 }
-input.addEventListener('input', inputHandler);
+function exect(value, delay) {
+  if (value) {
+    iziToast.show({
+      message: `✅ Fulfilled promise in ${delay}ms`,
+      backgroundColor: 'red',
+      position: 'topRight',
+      messageColor: 'white',
+      progressBar: false,
+      close: false,
+    });
+  } else {
+    iziToast.show({
+      message: `❌ Rejected promise in ${delay}ms`,
+      backgroundColor: 'red',
+      position: 'topRight',
+      messageColor: 'white',
+      progressBar: false,
+      close: false,
+    });
+  }
+}
+
 form.addEventListener('submit', submitHandler);
