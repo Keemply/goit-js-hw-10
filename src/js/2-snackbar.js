939a19ef611778441cbe454f15b25cedf32f6794
Keemply <144055348+Keemply@users.noreply.github.com>
2024-09-19 19:13:24 +0300

    fixed promise

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -33,34 +33,54 @@ function submitHandler(event) {
   new Promise((resolve, reject) => {
     setTimeout(() => {
       if (check) {
-        resolve(exect(check, delay));
+        resolve();
       } else {
-        reject(exect(check, delay));
+        reject();
       }
     }, delay);
-  });
+  })
+    .then(() => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        backgroundColor: 'red',
+        position: 'topRight',
+        messageColor: 'white',
+        progressBar: false,
+        close: false,
+      });
+    })
+    .catch(() => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        backgroundColor: 'red',
+        position: 'topRight',
+        messageColor: 'white',
+        progressBar: false,
+        close: false,
+      });
+    });
   form.reset();
 }
-function exect(value, delay) {
-  if (value) {
-    iziToast.show({
-      message: `✅ Fulfilled promise in ${delay}ms`,
-      backgroundColor: 'red',
-      position: 'topRight',
-      messageColor: 'white',
-      progressBar: false,
-      close: false,
-    });
-  } else {
-    iziToast.show({
-      message: `❌ Rejected promise in ${delay}ms`,
-      backgroundColor: 'red',
-      position: 'topRight',
-      messageColor: 'white',
-      progressBar: false,
-      close: false,
-    });
-  }
-}
+// function exect(value, delay) {
+//   if (value) {
+//     iziToast.show({
+//       message: `✅ Fulfilled promise in ${delay}ms`,
+//       backgroundColor: 'red',
+//       position: 'topRight',
+//       messageColor: 'white',
+//       progressBar: false,
+//       close: false,
+//     });
+//   } else {
+//     iziToast.show({
+//       message: `❌ Rejected promise in ${delay}ms`,
+//       backgroundColor: 'red',
+//       position: 'topRight',
+//       messageColor: 'white',
+//       progressBar: false,
+//       close: false,
+//     });
+//   }
+// }
 
 form.addEventListener('submit', submitHandler);
